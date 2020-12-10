8491c3af3c81967b1517d3817d9dd5b8f3a17ccf
Desmond Jeff <d.jeff@login2.ufhpc>
2020-12-10 00:11:58 -0500

    QoL improvements to stackbrightlines, probably some obsolete stuff in there regarding 3D mask testing. The new radialdistr code can make radial temperature distribution plots of the dataset and even converts between pixel units and physical distance. May make some adjustments as far as averaging the data, but it works very well so far.

---
+++ makeradialtexdistr.py
@@ -0,0 +1,72 @@
+from astropy.io import fits
+import numpy as np
+import astropy.units as u
+import math
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+mpl.interactive(True)
+
+plt.clf()
+
+def circle(data,ycenter,xcenter,rad):
+    edgex=[]
+    edgey=[]
+    for i in range(np.shape(data)[0]):
+        for j in range(np.shape(data)[1]):
+            if (j-xcenter)**2+(i-ycenter)**2==rad**2:
+                edgex.append(j)
+                edgey.append(i)
+    return np.vstack((edgex,edgey))
+home="/blue/adamginsburg/d.jeff/SgrB2DSreorg/field1/CH3OH/SgrB2S/OctReimage_z0_0002306756533745274_5-6mhzwidth_stdfixes/"
+texmap=home+"texmap_3sigma_allspw_withnans_weighted.fits"
+
+texmapdata=fits.getdata(texmap)*u.K
+
+dGC=8.34*u.kpc#per Meng et al. 2019 https://www.aanda.org/articles/aa/pdf/2019/10/aa35920-19.pdf
+
+cellsize=0.05*u.arcsec
+
+pixtophysicalsize=(np.tan(cellsize)*dGC).to('pc')
+
+print(pixtophysicalsize)
+
+texpeakpix=(73,56)#x-1, y-1 from DS9
+
+print(f'max: {texmapdata[texpeakpix[0],texpeakpix[1]]}')
+
+r=12
+#pixradius=math.ceil((0.08*u.pc/pixtophysicalsize).to(''))
+r_phys=r*pixtophysicalsize.to('pc')
+print(f'physical radius: {r_phys}')
+
+xpixs=np.arange(texpeakpix[0],(texpeakpix[0]+r))
+
+texinradius=[]
+xinradius=[]
+yinradius=[]
+centrtopix=[]
+
+for y in range(np.shape(texmapdata)[0]):
+    for x in range(np.shape(texmapdata)[1]):
+        if (y-texpeakpix[0])**2+(x-texpeakpix[1])**2 <= r**2:
+            texinradius.append(texmapdata[y,x].value)
+            xinradius.append(x)
+            yinradius.append(y)
+            centrtopix.append((np.sqrt((y-texpeakpix[0])**2+(x-texpeakpix[1])**2)*pixtophysicalsize).value)
+        else:
+            pass
+'''
+plt.scatter(centrtopix,texinradius)
+plt.xlabel('$d$ (pc)')
+plt.ylabel('$T_K$ (K)')
+'''
+#plt.show()
+
+edgepoints=circle(texmapdata,texpeakpix[0],texpeakpix[1],r)
+
+plt.imshow(texmapdata.value,origin='lower',vmax=550,vmin=10)
+plt.scatter(edgepoints[0],edgepoints[1],color='orange')
+#plt.scatter(xinradius,yinradius,color='orange')
+plt.show()
+
--- stackbrightlines.py
+++ stackbrightlines.py
@@ -7,13 +7,20 @@ import scipy.constants as cnst
 from astropy.io import fits
 import glob
 import matplotlib as mpl
+import os
 
 mpl.interactive(True)
 
-files=glob.glob("/blue/adamginsburg/d.jeff/SgrB2DSreorg/field1/CH3OH/SgrB2S/z0_0002306756533745274_testbox2_5-6mhzwidth/spectralslabs/km_s/*.fits")
-sum=sc.read(files[1])
-brightlines=[1,3,4,6,11]
+home="/blue/adamginsburg/d.jeff/SgrB2DSreorg/field1/CH3OH/SgrB2S/OctReimage_z0_0002306756533745274_5-6mhzwidth_stdfixes/"
+slabpath=home+"spectralslabs/km_s/*.fits"
+stackeddir=home+"3dmasktesting/"
 
+files=glob.glob(slabpath)
+'''Initialize the sum'''
+sum=sc.read(files[1])
+brightlines=[1,2,3,5,9]
+
+'''If line is the same as the initialized line, skip. Otherwise, add'''
 for line in brightlines:
     if files[line] == files[1]:
         print(f'{files[line]}=={files[1]}')
@@ -24,7 +31,17 @@ for line in brightlines:
         plt.plot(slab.spectral_axis,slab[:,75,60].value,color='gray',drawstyle='steps')
         sum=sum+slab
     
+'''Normalize by number of lines'''
 sum=sum/len(brightlines)
+
+if not os.path.isdir(stackeddir):
+    print(f'Creating 3D mask testing directory {stackeddir}')
+    os.mkdir(stackeddir)
+else:
+    print(f'3D mask testing directory {stackeddir} already exists')
+    pass
+    
+sum.write(stackeddir+'stackedlines1-3-4-6-11.fits')
 
 plt.plot(sum.spectral_axis,sum[:,75,60].value,color='red',drawstyle='steps')
 
